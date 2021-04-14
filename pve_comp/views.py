033a590799d28cc43be22c7699aaa0cc5893f38c
todo <chappy06060606@gmail.com>
2021-04-14 10:58:20 +0900

    画像アップロードフォームの追加

--- pve_comp/views.py
+++ pve_comp/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView, CreateView
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from pve_comp.forms import UploadForm
 from .models import ChapterStage
 from django.urls import reverse_lazy
@@ -8,16 +8,17 @@ from django.urls import reverse_lazy
 class PveCompView(TemplateView):
     template_name = 'pve_comp/pve_comp.html'
 
-    # def get(self, request):
-    #     return render(request, self.template_name)
-
 
 class UploadView(CreateView):
     model = ChapterStage
     form_class = UploadForm
     template_name = 'pve_comp/upload.html'
     success_url = reverse_lazy('pve_comp:upload')
-    
 
-    # def get(self, request):
-    #     return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        context = {
+            'message': '投稿完了',
+            'form': self.form_class
+        }
+        return render(request, self.template_name, context)
