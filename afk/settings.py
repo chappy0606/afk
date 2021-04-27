6addca686ae285a5eb67d972b25c20c4072e4ed9
todo <chappy06060606@gmail.com>
2021-04-27 23:46:02 +0900

    sqliteからmysqlに移行

--- afk/settings.py
+++ afk/settings.py
@@ -91,8 +91,10 @@ WSGI_APPLICATION = 'afk.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'afk',
+        'USER': 'root',
+        'PASSWORD': '',
     }
 }
 
