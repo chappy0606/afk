1702169f999e1b3d9c5d10aff54d0849b75965f7
todo <chappy06060606@gmail.com>
2021-05-25 05:16:48 +0900

    axios.getでchapter取得後表示まで完了

--- frontend/src/components/pages/PveComp.vue
+++ frontend/src/components/pages/PveComp.vue
@@ -1,24 +1,33 @@
 <template>
     <div>
-        <p>{{ getChapter() }}</p>
+        {{ getChapter() }}
+        <p>{{ state }}}</p>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent,} from "vue";
+import { defineComponent, reactive} from "vue";
 import axios from "axios";
 
-export default defineComponent({
+interface Chapter {
+    chpaterList: number;
+}
 
+export default defineComponent({
     setup() {
+        const state = reactive<Chapter>({
+            chpaterList: 0
+        });
         const getChapter = () => {
-            axios.get("http://127.0.0.1:8000/api/v1/chapter/")
-            .then(response => console.log(response))
-            .catch(error => console.log(error));
+            axios
+                .get("http://127.0.0.1:8000/api/v1/chapter/")
+                .then(response => (state.chpaterList = response.data))
+                .catch(error => console.log(error));
         };
-        return{
+        return {
             getChapter,
-        }
+            state
+        };
     }
-})
+});
 </script>
