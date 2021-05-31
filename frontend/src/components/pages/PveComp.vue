71c7b21508c19b15ae68483060fa5b1cd70aa844
todo <chappy06060606@gmail.com>
2021-05-31 22:17:36 +0900

    selectboxの追加

--- frontend/src/components/pages/PveComp.vue
+++ frontend/src/components/pages/PveComp.vue
@@ -1,37 +1,54 @@
 <template>
-
     <div>
-        <select v-model="selectedChapter">
-            <option disabled value="">chapter</option>
+        <select v-model="selectChapterStage">
+            <option disabled value="null">chapter...</option>
             <option v-for="chapter in state.chpaterList" :key="chapter.id">
-                {{ chapter.id }}
+                chapter{{ chapter.id }}
+            </option>
+        </select>
+        <select v-model="selectChapterStage">
+            <option disabled value="null">stage...</option>
+            <option v-for="stage in state.stageList" :key="stage.id">
+                stage{{ stage.id }}
             </option>
         </select>
     </div>
-
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import axios from 'axios'
 
-interface Chapter {
+interface ChapterStage {
     chpaterList: number
+    stageList: number
 }
 
 export default defineComponent({
     setup() {
-
-        const state = reactive<Chapter>({
-            chpaterList: 0
+        const state = reactive<ChapterStage>({
+            chpaterList: 0,
+            stageList: 0
         })
 
-        axios
-            .get('http://127.0.0.1:8000/api/v1/chapter/')
-            .then(response => (state.chpaterList = response.data))
+        const selectChapterStage: string = null
+
+        const getChapterStage = (): void => {
+            axios
+                .get('http://127.0.0.1:8000/api/v1/chapter/')
+                .then(response => (state.chpaterList = response.data)),
+            axios
+                .get('http://127.0.0.1:8000/api/v1/stage/')
+                .then(response => (state.stageList = response.data))
+        }
+
+        getChapterStage()
+
+        console.log(state.chpaterList)
 
         return {
-            state
+            state,
+            selectChapterStage
         }
     }
 })
