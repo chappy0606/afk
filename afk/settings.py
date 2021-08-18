17c6f188cfe201ea6ce1135e9803acffd63751d7
todo <chappy06060606@gmail.com>
2021-08-18 23:21:07 +0900

    更新なし

--- afk/settings.py
+++ afk/settings.py
@@ -69,7 +69,7 @@ REST_AUTH_SERIALIZERS = {
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
     'AUTH_COOKIE_HTTP_ONLY': True,
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=3)
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5)
 }
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
