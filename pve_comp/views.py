fa2b864230041e586d5cc091bcd9d7db4cf5048e
todo <chappy06060606@gmail.com>
2021-09-08 23:03:57 +0900

    ガード節

--- pve_comp/views.py
+++ pve_comp/views.py
@@ -25,55 +25,24 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def list(self, request, *args, **kwargs):
         if not request.GET:
-            print('クエリパラメータがない')
             return super().list(request, *args, **kwargs)
-        else:
-            print('クエリパラメータある')
-            if ('chapter_id' and 'stage_id') not in request.GET.keys():
-                print('keyがない')
-                return Response(status=status.HTTP_400_BAD_REQUEST)
-            else:
-                print('keyある')
-                if not (request.GET.get('chapter_id') and request.GET.get('stage_id')):
-                    print('両方に値がない')
-                    return Response(status=status.HTTP_400_BAD_REQUEST)
-                else:
-                    print('両方値がある')
-                    try:
-                        return super().list(request, *args, **kwargs)
-                    except ValueError:
-                        print('値が違う')
-                        return Response(status=status.HTTP_400_BAD_REQUEST)
-        # if request.GET:
-        #     print('クエリパラメータがある')
-        #     if ('chapter_id' and 'stage_id') in request.GET.keys():
-        #         print('keyが両方ある')
-        #         if request.GET.get('chapter_id') and request.GET.get('stage_id'):
-        #             print('両方に値がある')
-        #             try:
-        #                 return super().list(request, *args, **kwargs)
-        #             except ValueError:
-        #                 print('値が違う')
-        #                 return Response(status=status.HTTP_400_BAD_REQUEST)
-        #         else:
-        #             print('片方or両方値がない')
-        #             return Response(status=status.HTTP_400_BAD_REQUEST)
-        #     else:
-        #         print('keyが足りてない')
-        #         return Response(status=status.HTTP_400_BAD_REQUEST)
-        # else:
-        #     print('クエリパラメータない')
-        #     return super().list(request, *args, **kwargs)
+
+        if ('chapter_id' and 'stage_id') not in request.GET.keys():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        if not (request.GET.get('chapter_id') and request.GET.get('stage_id')):
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            return super().list(request, *args, **kwargs)
+        except ValueError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
     def get_queryset(self):
-        queryset = Post.objects.all()
-        if self.request.GET:
-            if self.request.GET.get('chapter_id') and self.request.GET.get('stage_id'):
-                chapter = self.request.GET.get('chapter_id')
-                stage = self.request.GET.get('stage_id')
-                queryset = Post.objects.filter(
-                    chapter_id=chapter, stage_id=stage)
-            else:
-                queryset = None
-
-        return queryset
+        if self.request.GET.get('chapter_id') and self.request.GET.get('stage_id'):
+            chapter = self.request.GET.get('chapter_id')
+            stage = self.request.GET.get('stage_id')
+            return Post.objects.filter(
+                chapter_id=chapter, stage_id=stage)
+        
+        return Post.objects.all()
