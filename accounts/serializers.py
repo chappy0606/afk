59fb0fe7755945fa0b6ed9b6e583bd0191e7baf5
todo <chappy06060606@gmail.com>
2021-09-17 18:49:45 +0900

    password_validationの削除

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,8 +2,13 @@ from rest_framework import serializers
 from accounts.models import User
 from django.utils.translation import gettext_lazy as _
 from dj_rest_auth.registration.serializers import RegisterSerializer
-from .password_validation import CustomValidator
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+from string import ascii_uppercase, ascii_lowercase, digits
 
+
+def contain_any(target, condition_list):
+    return any([i in target for i in condition_list])
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,9 +22,16 @@ class CustomRegisterSerializer(serializers.ModelSerializer, RegisterSerializer):
         model = User
         fields = ('username', 'password', 'email')
 
+
     def validate_password(self, password):
-        CustomValidator().password_validate(password)
+        if not all([contain_any(password, ascii_lowercase),
+                    contain_any(password, ascii_uppercase),
+                    contain_any(password, digits),
+                    len(password) >= 8]):
+    
+            raise ValidationError("パスワードは(大小英字、数字）全てを組み合わせて8文字以上に設定してください。")
         return password
+
     
     def validate(self, data):
         return data
