894d639c8cd2e2e64de69796de15927ba1bde3ac
todo <chappy06060606@gmail.com>
2021-11-24 06:19:15 +0900

    database template before migrate

--- relic_calculator/models.py
+++ relic_calculator/models.py
@@ -1,3 +1,18 @@
+from functools import total_ordering
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+class Quality(models.Model):
+    type = models.CharField(max_length=20)
+
+
+class Relic(models.Model):
+    name = models.CharField(max_length=50)
+    quality = models.ForeignKey(Quality, on_delete=models.CASCADE)
+    compornent1 = models.ForeignKey('self',blank=True,null=True)
+    compornent2 = models.ForeignKey('self', blank=True, null=True)
+    compornent3 = models.ForeignKey('self', blank=True, null=True)
+    compornent4 = models.ForeignKey('self', blank=True, null=True)
+    cost = models.IntegerField(validators=MinValueValidator(0))
+    total_price = models.IntegerField(validators=MinValueValidator(0))
