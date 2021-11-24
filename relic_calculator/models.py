d667d7a75ce225340423c5a4314c475c0515396f
todo <chappy06060606@gmail.com>
2021-11-24 23:52:36 +0900

    relic_calculator.models updata

--- relic_calculator/models.py
+++ relic_calculator/models.py
@@ -1,4 +1,3 @@
-from functools import total_ordering
 from django.db import models
 from django.core.validators import MinValueValidator
 
@@ -10,9 +9,13 @@ class Quality(models.Model):
 class Relic(models.Model):
     name = models.CharField(max_length=50)
     quality = models.ForeignKey(Quality, on_delete=models.CASCADE)
-    compornent1 = models.ForeignKey('self',blank=True,null=True)
-    compornent2 = models.ForeignKey('self', blank=True, null=True)
-    compornent3 = models.ForeignKey('self', blank=True, null=True)
-    compornent4 = models.ForeignKey('self', blank=True, null=True)
-    cost = models.IntegerField(validators=MinValueValidator(0))
-    total_price = models.IntegerField(validators=MinValueValidator(0))
+    compornent1 = models.ForeignKey(
+        'self', blank=True, null=True, on_delete=models.SET_NULL, related_name='compornent1_relic')
+    compornent2 = models.ForeignKey(
+        'self', blank=True, null=True, on_delete=models.SET_NULL, related_name='compornent2_relic')
+    compornent3 = models.ForeignKey(
+        'self', blank=True, null=True, on_delete=models.SET_NULL, related_name='compornent3_relic')
+    compornent4 = models.ForeignKey(
+        'self', blank=True, null=True, on_delete=models.SET_NULL, related_name='compornent4_relic')
+    cost = models.IntegerField(validators=[MinValueValidator(0)])
+    total_price = models.IntegerField(validators=[MinValueValidator(0)])
