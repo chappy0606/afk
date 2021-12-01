ffc63bc3e369709ed72756b8d4d94681f8c9ead4
todo <chappy06060606@gmail.com>
2021-12-01 11:21:50 +0900

    Succeeded in displaying the image acquired from api

--- frontend/src/components/pages/RelicCalcuator.vue
+++ frontend/src/components/pages/RelicCalcuator.vue
@@ -1,14 +1,42 @@
 <template>
-<div>
-    test
-</div>
+    <div>
+        <li v-for="(value,key) in relics" :key="key">
+            <img :src="value.icon" width="50" height="50">
+        </li>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import axios from '../../export'
+
+interface Relic {
+    id: string
+    enName: string
+    jaName: string
+    quality: string
+    compornent1: string
+    compornent2: string
+    compornent3: string
+    compornent4: string
+    cost: number
+    totalPrice: number
+    icon: string
+}
+interface Array {
+    relics: Relic[]
+}
+
 export default defineComponent({
     setup() {
-        console.log('aaa')
+        const relics = ref<Array[]>([])
+
+        axios.get('relic_calculator/relics').then(response => {
+            relics.value = response.data
+        })
+        return {
+            relics
+        }
     }
 })
 </script>
