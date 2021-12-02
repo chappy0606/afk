b01267b86618b89d547cb065aeb5a956450ca7d8
todo <chappy06060606@gmail.com>
2021-12-02 10:37:04 +0900

    Implementation of computed. Operation of relic to display

--- frontend/src/components/pages/RelicCalcuator.vue
+++ frontend/src/components/pages/RelicCalcuator.vue
@@ -1,42 +1,54 @@
 <template>
     <div>
-        <li v-for="(value,key) in relics" :key="key">
-            <img :src="value.icon" width="50" height="50">
+        <li v-for="(relic, key) in test" :key="key">
+            <img :src="relic.icon" width="50" height="50" :alt="relic.jaName" />
         </li>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, computed, ref } from 'vue'
 import axios from '../../export'
 
 interface Relic {
-    id: string
-    enName: string
-    jaName: string
-    quality: string
-    compornent1: string
-    compornent2: string
-    compornent3: string
-    compornent4: string
-    cost: number
-    totalPrice: number
-    icon: string
-}
-interface Array {
-    relics: Relic[]
+        id: string
+        enName: string
+        jaName: string
+        quality: string
+        compornent1: string
+        compornent2: string
+        compornent3: string
+        compornent4: string
+        cost: number
+        totalPrice: number
+        icon: string
 }
 
 export default defineComponent({
     setup() {
-        const relics = ref<Array[]>([])
+        const relics = ref<Relic[]>([])
 
-        axios.get('relic_calculator/relics').then(response => {
-            relics.value = response.data
+        axios
+            .get('relic_calculator/relics')
+            .then(response => {
+                relics.value = response.data
+            })
+            .catch(error => {
+                console.log(error.response)
+            })
+
+        const test = computed(() => {
+            return relics.value.filter((relic) => relic.quality.includes('Common'))
         })
+
         return {
-            relics
+            test
         }
     }
 })
 </script>
+<style>
+li {
+    list-style: none;
+}
+</style>
