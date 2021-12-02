89f7b146f613197ab8081766ee38ffbe2497f956
todo <chappy06060606@gmail.com>
2021-12-02 10:53:52 +0900

    add switch button

--- frontend/src/components/pages/RelicCalcuator.vue
+++ frontend/src/components/pages/RelicCalcuator.vue
@@ -1,5 +1,12 @@
 <template>
     <div>
+        <div class="category-nav">
+            <label> <input type="radio" value="" v-model="quality" />All </label>
+            <label> <input type="radio" value="Common" v-model="quality" />Common </label>
+            <label> <input type="radio" value="Rare" v-model="quality" />Rare </label>
+            <label> <input type="radio" value="Elite" v-model="quality" />Elite </label>
+        </div>
+
         <li v-for="(relic, key) in test" :key="key">
             <img :src="relic.icon" width="50" height="50" :alt="relic.jaName" />
         </li>
@@ -11,22 +18,23 @@ import { defineComponent, computed, ref } from 'vue'
 import axios from '../../export'
 
 interface Relic {
-        id: string
-        enName: string
-        jaName: string
-        quality: string
-        compornent1: string
-        compornent2: string
-        compornent3: string
-        compornent4: string
-        cost: number
-        totalPrice: number
-        icon: string
+    id: string
+    enName: string
+    jaName: string
+    quality: string
+    compornent1: string
+    compornent2: string
+    compornent3: string
+    compornent4: string
+    cost: number
+    totalPrice: number
+    icon: string
 }
 
 export default defineComponent({
     setup() {
         const relics = ref<Relic[]>([])
+        const quality = ref<string>('')
 
         axios
             .get('relic_calculator/relics')
@@ -38,11 +46,14 @@ export default defineComponent({
             })
 
         const test = computed(() => {
-            return relics.value.filter((relic) => relic.quality.includes('Common'))
+            return relics.value.filter(relic =>
+                relic.quality.includes(quality.value)
+            )
         })
 
         return {
-            test
+            test,
+            quality
         }
     }
 })
