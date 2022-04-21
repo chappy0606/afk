cc210c57fefeaae7a9c920a085a7b660b8767154
todo <chappy06060606@gmail.com>
2022-04-21 21:10:38 +0900

    Switch with radio button

--- react-frontend/src/components/pages/RelicCalcuator.tsx
+++ react-frontend/src/components/pages/RelicCalcuator.tsx
@@ -18,6 +18,8 @@ interface Relic {
 
 const RelicCalcuator = () => {
   const [relics, setRelic] = useState<Relic[]>([])
+  const [quality, setQuality] = useState<string>('Common')
+
   useEffect(() => {
     axios
       .get('https://192.168.10.14:8000/api/v1/relic_calculator/relics')
@@ -30,15 +32,46 @@ const RelicCalcuator = () => {
   return (
     <>
       <h2>聖物計算機</h2>
-      {relics.map((relic) => (
-        <img
-          key={relic.id}
-          src={relic.icon}
-          alt={relic.jaName}
-          width="50"
-          height="50"
-        ></img>
-      ))}
+      <div className="quality-nav">
+        <label>
+          <input
+            type="radio"
+            value="Common"
+            checked={quality === 'Common'}
+            onChange={(e) => setQuality(e.target.value)}
+          />
+          Common
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="Rare"
+            checked={quality === 'Rare'}
+            onChange={(e) => setQuality(e.target.value)}
+          />
+          Rare
+        </label>
+        <label>
+          <input
+            type="radio"
+            value="Elite"
+            checked={quality === 'Elite'}
+            onChange={(e) => setQuality(e.target.value)}
+          />
+          Elite
+        </label>
+      </div>
+      {relics
+        .filter((relic) => relic.quality === quality)
+        .map((relic) => (
+          <img
+            key={relic.id}
+            src={relic.icon}
+            alt={relic.jaName}
+            width="50"
+            height="50"
+          ></img>
+        ))}
     </>
   )
 }
